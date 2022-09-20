774d5988ea6249b661d997981600efa28c39e2b8
BrendenKingTLG <Brenden.king@tlgcohort.com>
2022-09-20 12:03:12 -0600

    test

--- src/test/java/com/tlglearing/FizzBuzzTest.java
+++ src/test/java/com/tlglearing/FizzBuzzTest.java
@@ -2,9 +2,15 @@ package com.tlglearing;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class FizzBuzzTest {
 
   @Test
   void valueOf() {
+    assertEquals(Set.of(FizzBuzz.FIZZ), FizzBuzz.valueOf(6));
   }
+
 }
